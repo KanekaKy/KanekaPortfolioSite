b0de9dfc0a33e085475ee0653513f24ed9ea6ecd
Kaneka <kaneka.ky@gmail.com>
2020-08-12 13:48:51 -0700

    Projects and Contact completed

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,50 @@
-import React, {Component} from 'react'; 
+import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 class Contact extends Component {
-    render(){
+    render() {
         return (
-            <div><h1>Contact Page</h1></div>
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2> KANEKA KY </h2>
+                        <img
+                            src="https://media-public.canva.com/MADnJnfLdWo/2/screen.svg"
+                            alt="avatar"
+                            style={{ height: '200px', paddingTop: '1em' }}
+                        />
+                        <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>Shut Up & Code</p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr />
+                        <div className="contact-List">
+                        <List>
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '30px', fontFamily:'Anton'}}>
+                                    <i className="fa fa-phone-square" aria-hidden="true"/>
+                                    315-751-1427
+                                    </ListItemContent>
+
+                            </ListItem>
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '30px', fontFamily:'Anton'}}> 
+                                <i className="fa fa-envelope-square" aria-hidden="true"/>
+                                Kaneka.Ky@gmail.com
+                                </ListItemContent>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '30px', fontFamily:'Anton'}}> 
+                                <i className="fa fa-skype" aria-hidden="true"/>
+                                KanekaKy
+                                </ListItemContent>
+                            </ListItem>
+            
+                        </List>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
